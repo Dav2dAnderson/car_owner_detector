143f559a732096fe143d0f28b6bf76ff3e4ac366
Dav2dAnderson <dav2danderson@gmail.com>
2025-08-23 10:46:27 +0500

    added: login, registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import search_user
+from .views import search_user, CustomRegisterView, CustomLoginView
 
 
 urlpatterns = [
-    path('search', search_user, name='search_user')
+    path('search/', search_user, name='search_user'),
+    path('register/', CustomRegisterView.as_view(), name="register_user"),
+    path('login/', CustomLoginView.as_view(), name='login_user')
 ]
+
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 from .models import CustomUser
 # Create your views here.
-
-
 
 
 def search_user(request):
@@ -18,6 +18,60 @@ def search_user(request):
     return render(request, "index.html", {"user": user})
     
 
+class CustomRegisterView(View):
+    def get(self, request):
+        return render(request, "register.html")
 
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        phone_number = request.POST.get("phone_number")
+        car = request.POST.get("car_name")
+        car_number = request.POST.get("car_number")
+        car_color = request.POST.get("car_color")
+
+        if password != confirm_password:
+            messages.error(request, "Parollar mos kelmadi.")
+            return render(request, "register.html")
+        
+        if CustomUser.objects.filter(username=username).exists():
+            messages.error(request, "Bunday foydalanuvchi nomi allaqachon mavjud")
+            return render(request, "register.html")
+        
+        if CustomUser.objects.filter(car_number=car_number).exists():
+            messages.error(request, "Bunday avtomobil raqami allaqachon mavjud")
+            return render(request, 'register.html')
+        
+        user = CustomUser.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name,
+                                              phone_number=phone_number, car=car, car_number=car_number, car_color=car_color)
+        
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+
+            messages.success(request, "Muvaffaqiyatli ro'yxatdan o'tdingiz")
+            return redirect('search_user')
+
+
+class CustomLoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "Hisobga kirdingiz")
+            return redirect('search_user')
+        else:
+            messages.error(request, "Foydalanuvchi nomi yoki parol noto'g'ri")
+            return render(request, 'login.html')
+        
     
     
